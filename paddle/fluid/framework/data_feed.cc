05ce8553594aacf07517af2803bdbc7ceb7ab4e5
danleifeng <danleifeng@163.com>
2022-12-29 09:00:52 +0000

    add dump_walk_path; test=develop

--- paddle/fluid/framework/data_feed.cc
+++ paddle/fluid/framework/data_feed.cc
@@ -2714,15 +2714,15 @@ void SlotRecordInMemoryDataFeed::DoWalkandSage() {
 #endif
 }
 
-#if defined(PADDLE_WITH_GPU_GRAPH) && defined(PADDLE_WITH_HETERPS)
 void SlotRecordInMemoryDataFeed::DumpWalkPath(std::string dump_path,
                                               size_t dump_rate) {
   VLOG(3) << "INTO SlotRecordInMemoryDataFeed::DumpWalkPath";
+#if defined(PADDLE_WITH_GPU_GRAPH) && defined(PADDLE_WITH_HETERPS)
   std::string path =
       string::format_string("%s/part-%03d", dump_path.c_str(), thread_id_);
   gpu_graph_data_generator_.DumpWalkPath(path, dump_rate);
-}
 #endif
+}
 
 #if defined(PADDLE_WITH_CUDA) && defined(PADDLE_WITH_HETERPS)
 void SlotRecordInMemoryDataFeed::BuildSlotBatchGPU(const int ins_num) {
