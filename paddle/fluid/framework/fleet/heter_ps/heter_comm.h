f787f4afe7334820d4db541446cdaf3cb7a7a519
DesmonDay <908660116@qq.com>
2023-06-09 18:04:31 +0800

    delete unused log

--- paddle/fluid/framework/fleet/heter_ps/heter_comm.h
+++ paddle/fluid/framework/fleet/heter_ps/heter_comm.h
@@ -605,10 +605,6 @@ class HeterComm {
           bool sage = false,
           bool slot = false) {
 
-    if (slot) {
-      VLOG(0) << "Enter scatter_inter_vals_by_all2all_common";
-    }
-
     auto &cache = storage_[gpu_id];
     auto &res = cache.shard_res;
 
